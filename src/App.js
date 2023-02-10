d22877e71fb92242ca723a0d158986c11f4912eb
saeidnobahari <saeidnobahari1@gmail.com>
2023-02-11 00:53:49 +0300

    add chatgpt component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ChatGPT from './components/chatgpt';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatGPT />
+    </div>
+  );
+}
+
+export default App;
